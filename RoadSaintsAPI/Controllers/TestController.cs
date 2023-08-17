b2c618bc344d54ea4280c71e9984f9e34216c300
emonsingha209 <emonsingha209@gmail.com>
2023-08-18 03:13:38 +0600

    test

---
+++ RoadSaintsAPI/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RoadSaintsAPI.Controllers
+{
+    [RoutePrefix("api/beng")]
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("world")]
+        public IHttpActionResult Oka() { 
+            return Ok("Hello, World!");
+        }
+
+        [HttpGet]
+        [Route("bal")]
+
+        public IHttpActionResult Bal()
+        {
+            return Ok("Heda");
+        }
+    }
+}
